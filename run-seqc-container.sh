238e8ba1d85cc27b641d139b9db36a3b15034bff
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-02-13 22:18:31 -0500

    Add

---
+++ run-seqc-container.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+USAGE: `basename $0` [options]
+
+    -k	absolute full path to AWS EC2 key pair (e.g. /home/chunj/dpeerlab-chunj.pem)
+
+EOF
+}
+
+while getopts "k:h" OPTION
+do
+    case $OPTION in
+        k) path_ec2_keypair=$OPTARG ;;        
+        h) usage; exit 1 ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [ -z "$path_ec2_keypair" ]
+then
+    usage
+    exit 1
+fi
+
+# extract only filename
+filename_ec2_keypair=`python -c "import os; print(os.path.basename('$path_ec2_keypair'));"`
+
+docker run \
+    -it --rm \
+    -e AWS_RSA_KEY=/root/${filename_ec2_keypair} \
+    --mount source=${path_ec2_keypair},target=/root/${filename_ec2_keypair},type=bind \
+    --mount source=~/.aws,target=/root/.aws,type=bind \
+    --entrypoint bash \
+    seqc
