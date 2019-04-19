8e03c5b8b7f36276d1bdb108b588b2af85aedbd5
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-04-18 22:55:21 -0400

    Update

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 version="0.2.3-alpha.2"
 s3_dest="s3://dp-lab-home/software"
@@ -6,7 +6,7 @@ s3_dest="s3://dp-lab-home/software"
 path_workdir=`mktemp -d`
 
 tar cvzf ${path_workdir}/seqc-${version}.tar.gz \
-    seqc-submit.sh
+    seqc-submit.sh seqc_submit_mjobs.py config/jobs.template.yml
 
 aws s3 cp ${path_workdir}/seqc-${version}.tar.gz ${s3_dest}/
 
