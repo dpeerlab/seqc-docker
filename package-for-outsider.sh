9c76c135d53e9b3d3c66312c100c94bf02cd8ca0
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-03-18 11:57:13 -0400

    Add package/instructions for outsider

---
+++ package-for-outsider.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+version="0.2.5"
+
+echo "Packaging seqc:${version}..."
+
+tar cvzf seqc-${version}.tar.gz \
+    seqc-submit.sh seqc-progress.sh seqc_submit_mjobs.py config/jobs.template.yml instructions.md
+
+echo "To unpack:"
+echo "tar xzf seqc-${version}.tar.gz -C seqc-${version}"
