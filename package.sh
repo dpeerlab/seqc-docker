bdc524e361543e0c17d535b21c1bfdcf9773a01d
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-10-07 22:44:59 -0400

    Update

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 hub="hisplan"
-version="0.2.6-rc6"
+version="0.2.6"
 
 echo "Packaging ${hub}/seqc:${version}..."
 
@@ -22,9 +22,21 @@ s3_dest="s3://dp-lab-home/software"
 
 path_workdir=`mktemp -d`
 
+# create installation script
+cat <<EOF > ${path_workdir}/install.sh
+#!/bin/bash
+
+aws s3 cp --quiet s3://dp-lab-home/software/seqc-${version}.tar.gz .
+mkdir -p seqc-${version}
+tar xzf seqc-${version}.tar.gz -C seqc-${version}
+
+echo "DONE."
+EOF
+
 tar cvzf ${path_workdir}/seqc-${version}.tar.gz \
     seqc-submit.sh seqc-progress.sh seqc_submit_mjobs.py show-ami-list.sh config/jobs.template.yml
 
 aws s3 cp ${path_workdir}/seqc-${version}.tar.gz ${s3_dest}/
+aws s3 cp ${path_workdir}/install.sh ${s3_dest}/install-seqc-${version}.sh
 
 rm -rf ${path_workdir}
